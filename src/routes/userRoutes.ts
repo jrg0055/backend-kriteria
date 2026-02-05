c201db1a0f4e8d357d1e3852da1b31fa90cd1f96
Jorge Ramos González <jrg0055@alu.medac.es>
2026-02-05 14:09:12 +0100

    getUsers eliminado de momento

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { getUsers, login, register } from "../controllers/userController";
+import { /*getUsers,*/ login, register } from "../controllers/userController";
 
 const router = Router();
 
 // Como se monta en /auth en index.ts, estas rutas serán /auth/register, etc.
 router.post('/register', register);
 router.post('/login', login);
-router.get("/", getUsers);
+/*router.get("/", getUsers);*/
 
 export default router;
